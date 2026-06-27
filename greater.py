4470bce85705f7df8f54b5b4583ea97b5b6ca630
sunitaoct26 <sunitaoct26@gmail.com>
2026-06-27 18:26:09 +0530

    added greater number program

---
+++ greater.py
@@ -0,0 +1,8 @@
+num1 = int(input("enter 1st number:"))
+num2 = int(input("enter 2nd number:"))
+if num1 > num2:
+    print(num1, "is greater")
+elif num2 > num1:
+    print(num2, "is greater")
+else:
+    print("both number are equal")       
